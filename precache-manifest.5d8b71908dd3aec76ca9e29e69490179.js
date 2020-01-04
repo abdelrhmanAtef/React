cfa1469e79de8337a342dd2fe482b22393a08a9e
Abdelrahman <bodyatef29@gmail.com>
2020-01-04 21:37:30 +0200

    Updates

---
+++ precache-manifest.5d8b71908dd3aec76ca9e29e69490179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabd15fec4609805231d76867e5fc1e1",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "69f343ab69bbd5ab667c",
+    "url": "/React/static/css/main.ca83a273.chunk.css"
+  },
+  {
+    "revision": "c69ec324687b93b0c4f9",
+    "url": "/React/static/js/2.fe01cefc.chunk.js"
+  },
+  {
+    "revision": "41dec1805a1138a227a108b795866cf1",
+    "url": "/React/static/js/2.fe01cefc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69f343ab69bbd5ab667c",
+    "url": "/React/static/js/main.22aca780.chunk.js"
+  },
+  {
+    "revision": "6da78ac28ccaa08f786f",
+    "url": "/React/static/js/runtime-main.a86431e8.js"
+  }
+]);
